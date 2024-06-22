0c3f99d60ea4b337341b0b9401887d0ae158ce18
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2024-06-22 11:09:44 -0600

    fix: convnext

--- experiments/e7_convnext_50_50.py
+++ experiments/e7_convnext_50_50.py
@@ -83,7 +83,6 @@ def main():
             model_name=config.CONVNEXT_50_50_FILENAME.replace("_", ""),
             loss_fn=nn.CrossEntropyLoss(),
             metrics=metrics,
-            vectorized_metrics=vector_metrics,
             lr=config.LR,
             scheduler_max_it=config.SCHEDULER_MAX_IT,
             per_class_metrics=vector_metrics,
